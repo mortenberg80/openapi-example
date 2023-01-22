ca8fbd0cca92d18914651e82b8cb81fe4c62a7f9
Morten Slaatten Berg <k01259@norsk-tipping.no>
2023-01-22 23:45:35 +0100

    Modified 18 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE3OTQ4OQ/spectral.js?branch_slug=master&token=2af85773-bd33-4370-a32f-7f8b2f026f04";
+export default { extends: ruleset };
--- docs/Welcome.md
+++ docs/Welcome.md
@@ -5,5 +5,3 @@ stoplight-id: 2r7397o1k25ee
 # Welcome
 
 This is an example project for exploring the OpenAPI specification, and the tooling that support it.
-
-Hello from CoP
